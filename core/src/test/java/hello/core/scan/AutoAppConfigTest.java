7914f67dfabc07b5b0df78eba5cf545649aa7cd3
강경지 <kangkj001@gmail.com>
2025-05-14 17:18:01 +0900

    Spring-Basic 20250514

--- core/src/test/java/hello/core/scan/AutoAppConfigTest.java
+++ core/src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -2,21 +2,29 @@ package hello.core.scan;
 
 import hello.AppConfig;
 import hello.core.AutoAppConfig;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
+import hello.core.order.OrderServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.assertj.core.api.ObjectAssert;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.*;
+
 
 public class AutoAppConfigTest {
 
     @Test
     void basicScan(){
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
-
         MemberService memberService = ac.getBean(MemberService.class);
         assertThat(memberService).isInstanceOf(MemberService.class);
+
+        OrderServiceImpl bean = ac.getBean(OrderServiceImpl.class);
+        MemberRepository memberRepository = bean.getMemberRepository();
+        System.out.println("memberRepository = "+ memberRepository);
     }
+
 }
