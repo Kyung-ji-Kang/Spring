fb3fe1f67bb59fe5b84a34c094355ca140a3cb21
강경지 <kangkj001@gmail.com>
2025-05-14 14:24:23 +0900

    Spring-Basic 20250514

--- core/src/main/java/hello/core/order/OrderService.java
+++ core/src/main/java/hello/core/order/OrderService.java
@@ -1,5 +1,9 @@
 package hello.core.order;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Component
 public interface OrderService {
     Order createOrder(Long memberId, String itemName, int itemPrice);
 
